d894abfd6126983b302b73a6884b193ca9c86abf
Darshana Perera <darshana.saluka.10@gmail.com>
2022-06-25 19:28:44 +0530

    digital clock added

--- src/Design/Design.js
+++ src/Design/Design.js
@@ -2,13 +2,27 @@ import React from "react";
 import DigitalClock from "./DigitalClock";
 import HeadText from "./HeadText";
 import Time from "./Time";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 export default function Design() {
+  const [state, setState] = React.useState(false);
+
+  const handleChange = (event) => {
+    setState(!state);
+  };
   return (
     <div>
       <HeadText />
-      <Time />
-      {/* <DigitalClock /> */}
+      <FormControlLabel
+        style={{ padding: "10px 0px" }}
+        control={
+          <Switch checked={state} onChange={handleChange} name="jason" />
+        }
+        label="Change Clock"
+      />
+      {state && <Time />}
+      {!state && <DigitalClock />}
     </div>
   );
 }
--- src/Design/SingleCircle.js
+++ src/Design/SingleCircle.js
@@ -5,7 +5,7 @@ import "react-circular-progressbar/dist/styles.css";
 export default function SingleCircle(prop) {
   const percentage = prop.data;
   return (
-    <div style={{ width: 100, height: 100, padding: "50px 20px" }}>
+    <div style={{ width: 100, height: 100, padding: "20px 20px" }}>
       <CircularProgressbar value={percentage} text={`${prop.txt}`} />
       <p style={{ margin: "10px" }}>{prop.name}</p>
     </div>
